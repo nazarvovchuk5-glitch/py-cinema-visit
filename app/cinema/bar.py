82d6ecb4d9db50b29e5ed3b6db6d3cbe89ca9063
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-04-18 00:04:00 +0300

    Solution1

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,5 @@
 class CinemaBar:
     @staticmethod
-    def sell_product(product, customer):
-        print(f"{product} was sold to {customer.name}")
+    def sell_product(product: str, customer: str | object) -> None:
+        name = customer.name if hasattr(customer, "name") else customer
+        print(f"Cinema bar sold {product} to {name}.")
--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
-from app.people.customer import Customer
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    customer_list = [Customer(name=customer["name"], food=customer["food"]) for customer in customers]
-    hall = CinemaHall(hall_number)
-    cleaner_instance = Cleaner(cleaner)
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str) -> None:
+    customer_list = [
+        Customer(name=customer["name"],
+                 food=customer["food"]) for customer in customers]
+    hall = CinemaHall(number=hall_number)
+    cleaner_instance = Cleaner(name=cleaner)
 
     for customer in customer_list:
         CinemaBar.sell_product(product=customer.food, customer=customer.name)
 
-    hall.movie_session(movie_name=movie, customers=customers, cleaning_staff=cleaner)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_list,
+        cleaning_staff=cleaner_instance)
